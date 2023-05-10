7dec895b37331cfa4de2ac71e6ecd677e7f7dbaa
omid-sar <mr.omid.sardari@gmail.com>
2023-05-10 16:26:42 -0400

    Fifth Commit

--- src/features/004_data_preprocessing.py
+++ src/features/004_data_preprocessing.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import sys
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import MinMaxScaler
 
-
-# -------------------------------------------------------------
-# 1. Read interim data
-# -------------------------------------------------------------
+# ------------------------------------ 1. Read Data -------------------------------------------
 
 sys.path.append("..")
 import utility.plot_settings
@@ -19,9 +18,7 @@ df = pd.read_pickle("../../data/interim/df.pkl")
 df["loan_status"] = df["loan_status"].map({"Fully Paid": 1, "Charged Off": 0})
 df.corr()["loan_status"].sort_values(ascending=False)[1:].plot(kind="barh")
 
-# -------------------------------------------------------------
-# 2. Data preprocessing
-# -------------------------------------------------------------
+# ------------------------------------ 2. Data Preprocessing ------------------------------------
 
 """
 The goal of this step is to prepare the data for the machine learning algorithms.
@@ -71,6 +68,13 @@ df["title"].nunique()
 # there are 63144 unique title.
 # There are many unique values in title column. So we will remove this column
 df.drop("title", axis=1, inplace=True)
+
+
+# 2.1.5
+# grade: is the loan grade. 
+# sub_grade: is the loan subgrade. 
+# grade is a part of sub_grade. So we will remove grade column
+df.drop("grade", axis=1, inplace=True)
 
 
 # 2.2 finding columns with missing values and fill/remove them
@@ -130,10 +134,92 @@ def fill_mort_acc(total_acc, mort_acc):
     else:
         return mort_acc
 # apply fill_mort_acc function to mort_acc column
-df["mort_acc"] = df.apply( lambda x: fill_mort_acc(x["total_acc"], x["mort_acc"]), axis=1))
+df["mort_acc"] = df.apply( lambda x: fill_mort_acc(x["total_acc"], x["mort_acc"]), axis=1)
 
 
-total_acc_avg.iloc[2]
-total_acc_avg.loc[2]
-total_acc_avg[1.0]
-total_acc_avg.index[0]
+# 2.2.3
+# remove rows with missing values in revol_util and pub_rec_bankruptcies columns
+df.dropna(inplace=True)
+
+
+# 2.3 categorical features and dummy variables
+print(df.select_dtypes(["object"]).columns)
+# there are 7 categorical features
+#['term', 'grade', 'sub_grade', 'home_ownership', 'verification_status',
+# 'purpose', 'initial_list_status', 'application_type', 'address']
+
+
+# 2.3.1
+# term: is the number of payments on the loan. Values are in months and can be either 36 or 60.
+# convert term column to numerical column
+term_values = {' 36 months': 36, ' 60 months': 60}
+df['term'] = df.term.map(term_values)
+df.term.unique()
+
+
+# 2.3.2
+# home_ownership: is the home ownership status provided by the borrower during registration.
+df.home_ownership.unique()
+# there are 6 unique values in home_ownership column.
+# we will group 'ANY' and 'NONE' values into 'OTHER' category
+df["home_ownership"] = df["home_ownership"].replace(["NONE", "ANY"], "OTHER")
+
+# 2.3.3
+# dummy variables for sub_grade, home_ownership, verification_status, purpose, initial_list_status, application_type   
+dummies_df = pd.get_dummies(df[["sub_grade", "home_ownership", "verification_status", "purpose", "initial_list_status", "application_type"]], drop_first=True)
+df = pd.concat([df.drop(["sub_grade", "home_ownership", "verification_status", "purpose", "initial_list_status", "application_type"], axis=1), dummies_df], axis=1)
+
+
+# 2.3.4
+# address: is the address provided by the borrower during registration.
+df.address.head()
+# extract zip code from address column and create a new column zip_code
+df["zip_code"] = df["address"].apply(lambda x: x[-5:])
+df["zip_code"].nunique()
+# there are 10 unique zip codes!
+# its really strange!
+# we will create dummy variables for zip_code column
+dummy_df = pd.get_dummies(df["zip_code"], drop_first=True)
+df = pd.concat([df.drop("zip_code", axis=1), dummy_df], axis=1)
+# remove address column
+df.drop("address", axis=1, inplace=True)
+
+
+#2.4 remove duplicate columns and rows
+
+# 2.4.1 find duplicate columns
+df_T = df.T
+duplicate_columns = df_T.duplicated()
+# remove duplicate columns
+df_T = df_T[~duplicate_columns]
+df = df_T.T
+
+# 2.4.2 find duplicate rows
+duplicate_rows = df.duplicated()
+# remove duplicate rows
+df = df[~duplicate_rows]
+
+# ------------------------------------ 3. Export Proceessed Data ------------------------------------
+
+df.to_pickle("../../data/processed/processed_data.pkl")
+
+
+# ------------------------------------ 3. Data Preprocessing ------------------------------------
+
+
+#3.1 Train Test split
+X = df.drop("loan_status", axis=1)
+y = df["loan_status"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
+
+#3.2 remove outliers
+# remove outliers in numerical features
+X_train = X_train[X_train['annual_inc'] <= 300000]
+X_train = X_train[X_train['dti'] <= 50]
+X_train = X_train[X_train['open_acc'] <= 50]
+X_train = X_train[X_train['total_acc'] <= 80]
+X_train = X_train[X_train['revol_util'] <= 120]
+X_train = X_train[X_train['revol_bal'] <= 300000]
+
+
+
