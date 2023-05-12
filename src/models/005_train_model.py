2a2f18ca7fbc28859f98c55bd6401e16624005c8
omid-sar <mr.omid.sardari@gmail.com>
2023-05-12 14:29:06 -0400

    Update

--- src/models/005_train_model.py
+++ src/models/005_train_model.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+from scipy import stats 
+from scipy.stats import randint, uniform
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import (accuracy_score, confusion_matrix, classification_report, roc_auc_score, roc_curve, auc)
 from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBClassifier
 
@@ -36,7 +38,7 @@ scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# ------------------------------------ 3. Train Model ---------------------------------------------------
+# ------------------------------------ 3. Train Model(XGBoost Classifier) ---------------------------------------------------
 
 
 # 3.1 Function to print the scores of the model
@@ -57,15 +59,64 @@ def print_score(true, pred, train=True):
         print(f"Confusion Matrix: \n {confusion_matrix(true, pred)}\n")
         print("------------------------------------------------------")
 
+# 3.2 Train Model
 
-# 3.2 Conver input and output data to numpy array
-X_train = np.array(X_train).astype('float32')
-y_train = np.array(y_train).astype
-X_test = np.array(X_test).astype
-y_test = np.array(y_test).astype
+# Define the parameter space
+param_dist = {
+    'n_estimators': randint(50, 500),
+    'max_depth': randint(1, 10),
+    'learning_rate': uniform(0.01, 0.6),
+}
+
+# Create a XGBClassifier instance
+xgb_clf = XGBClassifier(use_label_encoder=False, eval_metric='auc')  # or 'aucpr'
+
+# Create a RandomizedSearchCV instance
+xgb_cv = RandomizedSearchCV(
+    estimator=xgb_clf,
+    param_distributions=param_dist,
+    cv=3,  # Number of cross-validation folds
+    n_iter=50,  # Number of parameter settings to sample
+    scoring='roc_auc',  # Scoring metric
+    n_jobs=-1,  # Use all CPU cores
+    verbose=1,  # Verbosity level
+    random_state=   42  # For reproducibility
+)
+
+# Suppose you have training data in X_train and y_train
+# Fit the RandomizedSearchCV instance to the data
+xgb_cv.fit(X_train, y_train)
+
+# Print the best parameters
+print("Best parameters found: ", xgb_cv.best_params_)
+## Best parameters found:  {'learning_rate': 0.2499165830291533,
+##                                'max_depth': 3, 'n_estimators': 413}
 
 
-# ------------------------------------ XGBoost Classifier ---------------------------------------------------
-xgb_clf = XGBClassifier()
 
-``
+# ------------------------------------ 4. Evaluate Model ---------------------------------------------------
+
+# Best parameters found in previous search
+best_params = {'learning_rate': 0.2499165830291533, 'max_depth': 3, 'n_estimators': 413}
+
+# Create a new XGBClassifier instance with the best parameters
+best_model = XGBClassifier(use_label_encoder=False, eval_metric='aucpr', **best_params)
+
+# Train the model on your data
+best_model.fit(X_train, y_train)
+
+# Now you can use the trained model to make predictions
+
+y_train_pred = best_model.predict(X_train)
+y_test_pred = best_model.predict(X_test)
+
+print_score(y_train, y_train_pred, train=True)
+print_score(y_test, y_test_pred, train=False)
+
+# And evaluate these predictions using your preferred evaluation metric
+
+
+
+
+
+
