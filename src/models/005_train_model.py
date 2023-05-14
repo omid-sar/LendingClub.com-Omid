efcf071e7de3a5c00fd9b107da8d25be73a10636
omid-sar <mr.omid.sardari@gmail.com>
2023-05-14 18:27:39 -0400

    update

--- src/models/005_train_model.py
+++ src/models/005_train_model.py
@@ -16,8 +16,8 @@ from sklearn.metrics import (
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
 from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
-
 # ------------------------------------ 1. Read Data ---------------------------------------------------
 
 
@@ -29,7 +29,7 @@ df = pd.read_pickle("../../data/processed/processed_data.pkl")
 # Split Data and Remove Outliers from Train Data and Feature Scaling
 
 # 2.1 Split Data to train and test
-train, test = train_test_split(df, test_size=0.33, random_state=42)
+train, test = train_test_split(df, test_size=0.03, random_state=42)
 
 # 2.2 remove outliers from train data
 train = train[train["annual_inc"] <= 300000]
@@ -96,7 +96,6 @@ xgb_cv = RandomizedSearchCV(
     random_state=42,  # For reproducibility
 )
 
-# Suppose you have training data in X_train and y_train
 # Fit the RandomizedSearchCV instance to the data
 xgb_cv.fit(X_train, y_train)
 
@@ -108,27 +107,25 @@ print("Best parameters found: ", xgb_cv.best_params_)
 
 """
 
+# Best parameters found in previous search
 
-#  4. Evaluate Model
-# 4.1 Best parameters found in previous search
 best_params = {"learning_rate": 0.2499165830291533, "max_depth": 3, "n_estimators": 413}
-
 # Create a new XGBClassifier instance with the best parameters
 best_model = XGBClassifier(use_label_encoder=False, eval_metric="auc", **best_params)
-
 # Train the model on your data
 best_model.fit(X_train, y_train)
-
 # Now you can use the trained model to make predictions
-
 y_train_pred = best_model.predict(X_train)
 y_test_pred = best_model.predict(X_test)
+
+
+#  3.3  Evaluate Model (XGBoost)
 
 print_score(y_train, y_train_pred, train=True)
 print_score(y_test, y_test_pred, train=False)
 
 
-# 4.2 Plot roc curve
+# 3.3.1  Plot roc curve
 y_scores = best_model.predict_proba(X_test)[:, 1]
 # Compute the false positive rate, true positive rate, and thresholds
 fpr, tpr, thresholds = roc_curve(y_test, y_scores)
@@ -139,7 +136,7 @@ roc_display.plot()
 # Show the plot
 plt.show()
 
-# 4.3 Create roc_auc_score dictionary for model comparison
+# 3.3.2 Create roc_auc_score dictionary for model comparison
 scores_dict = {
     "XGBoost": {
         "Train": roc_auc_score(y_train, best_model.predict(X_train)),
@@ -147,4 +144,63 @@ scores_dict = {
     },
 }
 
-# ------------------------------------ 5.
+# ------------------------------------ 4. Random Forest Classifier ---------------------------------------------------
+
+# 4.1 Train Model(Random Forest)
+# Define the parameter space
+param_dist = {
+    'n_estimators': range(100, 500),
+    'max_depth': [None] + list(range(5, 50)),
+    'min_samples_split': range(2, 20),
+    'min_samples_leaf': range(1, 20),
+}
+
+# Create a RandomForestClassifier instance
+rf_clf = RandomForestClassifier()
+
+# Create a RandomizedSearchCV instance
+rf_random_search = RandomizedSearchCV(estimator=rf_clf,
+                                        param_distributions=param_dist,
+                                        n_iter=50,
+                                        cv=3,
+                                        verbose=3,
+                                        random_state=42,
+                                        n_jobs=-1)
+
+ # Fit the RandomizedSearchCV instance to the data
+rf_random_search.fit(X_train, y_train)
+
+# Print the best parameters
+print("Best parameters found: ", rf_random_search.best_params_)
+## Best parameters found:  {'n_estimators': {}
+
+# Best parameters found in previous search
+best_params = {'n_estimators': 100, 'min_samples_split': 2, 'min_samples_leaf': 1, 'max_depth': 49}
+best_model = RandomForestClassifier()
+best_model.fit(X_train, y_train)
+y_train_pred = best_model.predict(X_train)
+y_test_pred = best_model.predict(X_test)
+
+#  4.2  Evaluate Model (Random Forest)
+
+print_score(y_train, y_train_pred, train=True)
+print_score(y_test, y_test_pred, train=False)
+
+# 4.2.1  Plot roc curve
+y_scores = best_model.predict_proba(X_test)[:, 1]
+# Compute the false positive rate, true positive rate, and thresholds
+fpr, tpr, thresholds = roc_curve(y_test, y_scores)
+# Create the RocCurveDisplay object
+roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr)
+# Plot the ROC curve
+roc_display.plot()
+# Show the plot
+plt.show()
+
+# 4.2.2 add roc_auc_score dictionary for model comparison
+scores_dict["Random Forest"] = { Train: roc_auc_score(y_train, best_model.predict(X_train)),
+                                Test: roc_auc_score(y_test, best_model.predict(X_test))}
+
+# ------------------------------------ 5. Neural Network Classifier ---------------------------------------------------
+
+# 5.1 Train Model(Neural Network)
